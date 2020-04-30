f2f99d7859a6ad53c95b05e5a9e4b33d1c0e6272
Viet Anh Le <mc.vietanh@gmail.com>
2020-05-01 01:07:19 +0700

    Updates

---
+++ precache-manifest.b5d18e52677488150d2218f5d0d66a2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf732f754a8b76b3357803d05963884",
+    "url": "/react-ant-dashboard/index.html"
+  },
+  {
+    "revision": "2a3f95f9e8383eee6db3",
+    "url": "/react-ant-dashboard/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "5ae97e01f01f27e1aea1",
+    "url": "/react-ant-dashboard/static/css/main.b0338631.chunk.css"
+  },
+  {
+    "revision": "2a3f95f9e8383eee6db3",
+    "url": "/react-ant-dashboard/static/js/2.02b9ec82.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-ant-dashboard/static/js/2.02b9ec82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae97e01f01f27e1aea1",
+    "url": "/react-ant-dashboard/static/js/main.03baa7b3.chunk.js"
+  },
+  {
+    "revision": "a553a6edd413e531c9e6",
+    "url": "/react-ant-dashboard/static/js/runtime-main.3aef8e6d.js"
+  }
+]);
